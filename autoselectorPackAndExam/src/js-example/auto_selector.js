380dbc3b15826f2c53c1b8bb45718eb64795bdfd
Ubuntu <ubuntu@ip-172-31-31-202.ap-northeast-2.compute.internal>
2018-04-25 10:28:30 +0000

    Click & Drag 구현

--- autoselectorPackAndExam/src/js-example/auto_selector.js
+++ autoselectorPackAndExam/src/js-example/auto_selector.js
@@ -48,8 +48,13 @@ annotorious.plugin.autoSelector.Selector.prototype.init = function(annotator, ca
   this._enabled = false;
 
   /** @private **/
+  this._drawLocking = false;
+
+  /** @private **/
   this._mouseUpListener;
 
+  /** @private **/
+  this._opposite;
 }
 
 /*
@@ -57,12 +62,54 @@ annotorious.plugin.autoSelector.Selector.prototype.init = function(annotator, ca
  */
 annotorious.plugin.autoSelector.Selector.prototype._attachListeners = function() {
   var self = this; 
+
+  /* mouse dragging listener */
+  var moveCount = 0;
+  this._mouseMoveListener = function(event) {
+  self._g2d.lineWidth = 6;
+    
+    moveCount += 1;
+    //console.log("getMoveCount",moveCount);
+    if (self._enabled && moveCount > 50) {
+      self._drawLocking = true; // custom draw Lock.
+
+      self._opposite = (event.offsetX == undefined) ? 
+        { x: event.layerX, y: event.layerY } : 
+        { x: event.offsetX, y: event.offsetY };
+      console.log("getOpposite", self._opposite);
+      self._g2d.clearRect(0, 0, self._canvas.width, self._canvas.height);
+      
+      var top, left, bottom, right;
+      if (self._opposite.y > self._anchor.y) {
+        top = self._anchor.y;
+        bottom = self._opposite.y;
+      } else {
+        top = self._opposite.y;    
+        bottom = self._anchor.y;
+      }
+      
+      if (self._opposite.x > self._anchor.x) {
+        right = self._opposite.x;
+        left = self._anchor.x;
+      } else {
+        right = self._anchor.x;   
+        left = self._opposite.x;     
+      }
+      var width = right - left;
+      var height = bottom - top;
+      self._g2d.strokeStyle = '#ffffff';
+      self._g2d.strokeRect(left + 0.5, top + 0.5, width, height);
+    }
+  };
+  this._canvas.addEventListener('mousemove', this._mouseMoveListener);
+
+  /* push up linsten */
   this._mouseUpListener = function(event){
     var shape = self.getShape();
+    
       if(shape){
-        self._enable = false;
         self.drawRect(self._anchor.x, self._anchor.y);
-        
+        self._enabled = false;
         try{
           self._annotator.fireEvent("onSelectionCompleted", 
           {mouseEvent:event, shape:shape, viewportBounds: self.getViewportBounds()});
@@ -74,9 +121,11 @@ annotorious.plugin.autoSelector.Selector.prototype._attachListeners = function()
         }
       } else {
         self._annotator.fireEvent("onSelectionCanceled");
-      }		
+      }
+      self._drawLocking = false;
   };
-  this._canvas.addEventListener('mouseup', this._mouseUpListener);	
+  this._canvas.addEventListener('mouseup', this._mouseUpListener);
+
 }
 
 
@@ -85,6 +134,7 @@ annotorious.plugin.autoSelector.Selector.prototype._attachListeners = function()
  */
 annotorious.plugin.autoSelector.Selector.prototype._detachListeners = function() {
   var self = this;
+  console.log("detache???")
   if (this._mouseMoveListener) {
      this._canvas.removeEventListener("mousemove", self._mouseMoveListener);
   }
@@ -101,25 +151,24 @@ annotorious.plugin.autoSelector.Selector.prototype.getName = function() {
 /*
  * become view for area that is selection sample.
  */
-annotorious.plugin.autoSelector.Selector.prototype.drawRect = function(click_x, click_y){
+annotorious.plugin.autoSelector.Selector.prototype.drawRect = function (click_x, click_y) {
+  if (this._drawLocking == false) {
+    this._g2d.strokeStyle = "#35E5F1"; // 청색
+    this._g2d.lineWidth = 6;
 
-  this._g2d.strokeStyle = "#35E5F1";
-  this._g2d.lineWidth = 6;
+    var x = click_x - 240;
+    var y = click_y - 240;
+    var w = 480;
+    var h = 480;
 
-  var x = click_x - 240;
-  var y = click_y - 240;
-  var w = 480;
-  var h = 480;
-
-  if( x < 0 ){
-    x = 0;
+    if (x < 0) {
+      x = 0;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+    this._g2d.strokeRect(x, y, w, h);
   }
-  if( y < 0) {
-    y = 0;
-  }
-  this._g2d.strokeRect(x, y, w, h);
-  return {x : x, y : y, w : w, h : h};
-  
 }
 
 annotorious.plugin.autoSelector.Selector.prototype.clearRect = function(){
@@ -133,6 +182,8 @@ annotorious.plugin.autoSelector.Selector.prototype.getSupportedShapeType = funct
 annotorious.plugin.autoSelector.Selector.prototype.startSelection = function(x, y) {
   console.log("start autoSelect!!");
   this._enabled = true;
+  this._opposite = {x : 0,
+                    y : 0}; // 초기화
   this._anchor = { x : x,
                    y : y};
   this._attachListeners();
@@ -169,12 +220,30 @@ annotorious.plugin.autoSelector.Selector.prototype.getShape = function() {
     y = 1 - (480 / this._canvas.height);
   }
 
-  var testmetry = { x: x, 
-                    y: y, 
-                    width: 480 / this._canvas.width, 
-                    height: 480 / this._canvas.height};
+  var autoRect = { x: x, 
+                   y: y, 
+                   width: 480 / this._canvas.width, 
+                   height: 480 / this._canvas.height};
+
+  console.log("coords length: ", this._coords);
+  if(this._coords == null){
+    console.log("coords null");
+  }
   
-  return { type: 'rect', geometry: { x: testmetry.x, y: testmetry.y, width: testmetry.width, height: testmetry.height } };
+  if (this._opposite &&
+    (Math.abs(this._opposite.x - this._anchor.x) > 3) &&
+    (Math.abs(this._opposite.y - this._anchor.y) > 3)) {
+    console.log("changed drag points")
+    var viewportBounds = this.getViewportBounds();
+    var item_anchor = this._annotator.toItemCoordinates({ x: viewportBounds.left, y: viewportBounds.top });
+    var item_opposite = this._annotator.toItemCoordinates({ x: viewportBounds.right - 1, y: viewportBounds.bottom - 1 });
+
+    return { type: 'rect', geometry: { x: item_anchor.x, y: item_anchor.y, width: item_opposite.x - item_anchor.x, height: item_opposite.y - item_anchor.y } };
+  } else {
+    return { type: 'rect', geometry: { x: autoRect.x, y: autoRect.y, width: autoRect.width, height: autoRect.height } };
+  }
+
+
 }
 
 
@@ -200,49 +269,51 @@ annotorious.plugin.autoSelector.Selector.prototype.getViewportBounds = function(
   }
 
  
-  var x = { top: top, right: right, bottom: bottom, left: left};
-  // var x = { top: 0, right: 0.2, bottom: 0.2, left: 0}; //test
-  console.log("getViewBinding",x);
-  return x 
-}
-
-
-/** no more to need.
- 
-annotorious.plugin.autoSelector.Selector.prototype.drawShape = function(g2d, shape, highlight) {
+  var autoRect = { top: top, right: right, bottom: bottom, left: left};
   
-  // TagBox UI Rectangle
-  var self = this;
-  console.log("drawShape!!", shape);
-  var x = shape.geometry.points[0].x - 240;
-  var y = shape.geometry.points[0].y - 240;
-  if( x < 0 ){
-    x = 0;
-  }
-  else if ( x > this._canvas.width){
-    x = this._canvas.width;
-  }
-    
-  if( y < 0) {
-    y = 0;
-  }
+  /** dragging event **/
+  if (this._opposite.x + this._opposite.y > 0) {
+    if (this._opposite.x > this._anchor.x) {
+      right = this._opposite.x;
+      left = this._anchor.x;
+    } else {
+      right = this._anchor.x;
+      left = this._opposite.x;
+    }
 
-  console.log("get start drawShape");
-  var keyBuff = { x : x,
-                  y : y,
-                  w : 480,
-                  h : 480};
+    var top, bottom;
+    if (this._opposite.y > this._anchor.y) {
+      top = this._anchor.y;
+      bottom = this._opposite.y;
+    } else {
+      top = this._opposite.y;
+      bottom = this._anchor.y;
+    }
 
-  this._rectArray.push(keyBuff);
-
-  g2d.lineWidth = 2.3;
-  g2d.strokeStyle = "#FA0101";
-  g2d.beginPath();
-  for(var i = 0; i < this._rectArray.length; i++){
-    console.log("drawStarted");
-    g2d.strokeRect(self._rectArray[i].x, self._rectArray[i].y, self._rectArray[i].w, self._rectArray[i].h);
-
+    return { top: top, right: right, bottom: bottom, left: left };
+  } else {
+    return autoRect;
   }
 }
 
-**/
+annotorious.plugin.autoSelector.Selector.prototype.drawShape = function(g2d, shape, highlight) {
+  console.log("DrawShape!!!");
+
+  if (shape.type == 'rect') {
+    var color, lineWidth;
+    if (highlight) {
+      color = '#fff000';
+      lineWidth = 1.2;
+    } else {
+      color = '#ffffff';
+      lineWidth = 1;
+    }
+
+    var geom = shape.geometry;
+    g2d.strokeStyle = '#000000';
+    g2d.lineWidth = lineWidth;
+    g2d.strokeRect(geom.x + 0.5, geom.y + 0.5, geom.width + 1, geom.height + 1);
+    g2d.strokeStyle = color;
+    g2d.strokeRect(geom.x + 1.5, geom.y + 1.5, geom.width - 1, geom.height - 1);
+  }
+}
